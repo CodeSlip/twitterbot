bb8525d49e613b15513870dc4515f3f830631f23
CodeSlip <jgtissera@gmail.com>
2017-11-17 15:58:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 //config.js
 /** TWITTER APP CONFIGURATION
- * consumer_key XFoXfOrdEIbYAgOj2PJvtF3mw
- * consumer_secret nckAkXGWwkfjaWdbZJCxmq0SAvdyDCNoeazatdEHp0PRavqDKq
- * access_token 97534069-FdrA2mkogpsPggfCtBdxFQ7U2IuTKpOWRk0FZlMA6
- * access_token_secret 0cGP6OConpGi7SKXpy6N9bM9lFs3sA6WcNaf7LkgHi4Ep
+ * consumer_key 
+ * consumer_secret 
+ * access_token 
+ * access_token_secret 
  */
 module.exports = {
-  consumer_key: 'XFoXfOrdEIbYAgOj2PJvtF3mw',
-  consumer_secret: 'nckAkXGWwkfjaWdbZJCxmq0SAvdyDCNoeazatdEHp0PRavqDKq',
-  access_token: '97534069-FdrA2mkogpsPggfCtBdxFQ7U2IuTKpOWRk0FZlMA6',
-  access_token_secret: '0cGP6OConpGi7SKXpy6N9bM9lFs3sA6WcNaf7LkgHi4Ep'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 }
